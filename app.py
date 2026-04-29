c190a9e4bcb4b8d313a3542ccc3fbf43565bc6b6
ForsuEliza <ezis134@gmail.com>
2026-04-29 14:39:03 +0300

    Sākuma lapas izveide.

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/vulkani')
+def vulkani():
+    db = get_db()
+    vulkani = db.execute("SELECT * FROM vulkani").fetchall()
+    return render_template('vulkani.html', vulkani=vulkani)
+
+
+@app.route('/vulkani/<int:id>')
+def vulkans(id):
+    db = get_db()
+    vulkans = db.execute("SELECT * FROM vulkani WHERE id=?", (id,)).fetchone()
+    return render_template('vulkans.html', vulkans=vulkans)
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    db = get_db()
+
+    if request.method == 'POST':
+        db.execute("""
+        UPDATE vulkani SET
+        name=?, height=?, diameter=?, country=?, continent=?, type=?, last_eruption=?, damage=?, activity=?
+        WHERE id=?
+        """, (
+            request.form['name'],
+            request.form['height'],
+            request.form['diameter'],
+            request.form['country'],
+            request.form['continent'],
+            request.form['type'],
+            request.form['last_eruption'],
+            request.form['damage'],
+            request.form['activity'],
+            id
+        ))
+        db.commit()
+        return redirect(url_for('vulkani'))
+
+    vulkans = db.execute("SELECT * FROM vulkani WHERE id=?", (id,)).fetchone()
+    return render_template('edit.html', vulkans=vulkans)
+
+
+@app.route('/edit_activity/<int:id>', methods=['POST'])
+def edit_activity(id):
+    db = get_db()
+    db.execute("UPDATE vulkani SET activity=? WHERE id=?", 
+               (request.form['activity'], id))
+    db.commit()
+    return redirect(url_for('vulkans', id=id))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

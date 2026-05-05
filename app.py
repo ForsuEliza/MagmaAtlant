12c8dd179b45c4f292b6e9c9fe0a41cdccaecec8
ForsuEliza <ezis134@gmail.com>
2026-05-05 19:35:13 +0300

    Pabeigta mājaslapa

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 import sqlite3
 from pathlib import Path
 from werkzeug.utils import secure_filename
@@ -32,12 +32,9 @@ def vulkani():
                Vulkani_tips.type,
                Vulkani_aktivitate.eruption_reason
         FROM Vulkani
-        LEFT JOIN Vulkani_kontinenti
-            ON Vulkani.id_continent = Vulkani_kontinenti.id
-        LEFT JOIN Vulkani_tips
-            ON Vulkani.id_type = Vulkani_tips.id
-        LEFT JOIN Vulkani_aktivitate
-            ON Vulkani.id_activity = Vulkani_aktivitate.id
+        LEFT JOIN Vulkani_kontinenti ON Vulkani.id_continent = Vulkani_kontinenti.id
+        LEFT JOIN Vulkani_tips ON Vulkani.id_type = Vulkani_tips.id
+        LEFT JOIN Vulkani_aktivitate ON Vulkani.id_activity = Vulkani_aktivitate.id
     """).fetchall()
 
     conn.close()
@@ -54,26 +51,21 @@ def vulkans(id):
                Vulkani_tips.type,
                Vulkani_aktivitate.eruption_reason
         FROM Vulkani
-        LEFT JOIN Vulkani_kontinenti
-            ON Vulkani.id_continent = Vulkani_kontinenti.id
-        LEFT JOIN Vulkani_tips
-            ON Vulkani.id_type = Vulkani_tips.id
-        LEFT JOIN Vulkani_aktivitate
-            ON Vulkani.id_activity = Vulkani_aktivitate.id
+        LEFT JOIN Vulkani_kontinenti ON Vulkani.id_continent = Vulkani_kontinenti.id
+        LEFT JOIN Vulkani_tips ON Vulkani.id_type = Vulkani_tips.id
+        LEFT JOIN Vulkani_aktivitate ON Vulkani.id_activity = Vulkani_aktivitate.id
         WHERE Vulkani.id = ?
     """, (id,)).fetchone()
 
-    aktivitate = conn.execute(
-        "SELECT * FROM Vulkani_aktivitate"
-    ).fetchall()
+    if vulkans is None:
+        conn.close()
+        abort(404)
+
+    aktivitate = conn.execute("SELECT * FROM Vulkani_aktivitate").fetchall()
 
     conn.close()
 
-    return render_template(
-        "vulkans.html",
-        vulkans=vulkans,
-        aktivitate=aktivitate
-    )
+    return render_template("vulkans.html", vulkans=vulkans, aktivitate=aktivitate)
 
 
 @app.route("/edit_activity/<int:id>", methods=["POST"])
@@ -82,9 +74,7 @@ def edit_activity(id):
 
     conn.execute("""
         UPDATE Vulkani
-        SET id_activity = ?,
-            last_eruption = ?,
-            damage = ?
+        SET id_activity = ?, last_eruption = ?, damage = ?
         WHERE id = ?
     """, (
         request.form.get("activity"),
@@ -99,15 +89,11 @@ def edit_activity(id):
     return redirect(url_for("vulkans", id=id))
 
 
-@app.route("/delete/<int:id>")
+@app.route("/delete/<int:id>", methods=["POST"])
 def delete(id):
     conn = get_db()
 
-    conn.execute(
-        "DELETE FROM Vulkani WHERE id = ?",
-        (id,)
-    )
-
+    conn.execute("DELETE FROM Vulkani WHERE id = ?", (id,))
     conn.commit()
     conn.close()
 
@@ -119,48 +105,37 @@ def add():
     conn = get_db()
 
     if request.method == "POST":
+        name = request.form.get("name")
+
+        if not name:
+            conn.close()
+            return "Nosaukums obligāts", 400
 
         image_file = request.files.get("image")
         filename = None
 
-        if image_file and image_file.filename != "":
+        if image_file and image_file.filename:
             filename = secure_filename(image_file.filename)
-
-            image_path = os.path.join(
-                app.config["UPLOAD_FOLDER"],
-                filename
-            )
-
+            image_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             image_file.save(image_path)
 
         conn.execute("""
             INSERT INTO Vulkani
-            (
-                name,
-                height,
-                diameter,
-                images,
-                country,
-                id_continent,
-                id_type,
-                id_activity,
-                coordinates,
-                last_eruption,
-                damage
-            )
+            (name, height, diameter, images, country, id_continent,
+             id_type, id_activity, coordinates, last_eruption, damage)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """, (
-            request.form["name"],
-            request.form["height"],
-            request.form["diameter"],
+            name,
+            request.form.get("height"),
+            request.form.get("diameter"),
             filename,
-            request.form["country"],
-            request.form["continent"],
-            request.form["type"],
-            request.form["activity"],
-            request.form["coordinates"],
-            request.form["last_eruption"],
-            request.form["damage"]
+            request.form.get("country"),
+            request.form.get("continent"),
+            request.form.get("type"),
+            request.form.get("activity"),
+            request.form.get("coordinates"),
+            request.form.get("last_eruption"),
+            request.form.get("damage")
         ))
 
         conn.commit()
@@ -168,26 +143,16 @@ def add():
 
         return redirect(url_for("vulkani"))
 
-    kontinenti = conn.execute(
-        "SELECT * FROM Vulkani_kontinenti"
-    ).fetchall()
-
-    tipi = conn.execute(
-        "SELECT * FROM Vulkani_tips"
-    ).fetchall()
-
-    aktivitate = conn.execute(
-        "SELECT * FROM Vulkani_aktivitate"
-    ).fetchall()
+    kontinenti = conn.execute("SELECT * FROM Vulkani_kontinenti").fetchall()
+    tipi = conn.execute("SELECT * FROM Vulkani_tips").fetchall()
+    aktivitate = conn.execute("SELECT * FROM Vulkani_aktivitate").fetchall()
 
     conn.close()
 
-    return render_template(
-        "add.html",
-        kontinenti=kontinenti,
-        tipi=tipi,
-        aktivitate=aktivitate
-    )
+    return render_template("add.html",
+                           kontinenti=kontinenti,
+                           tipi=tipi,
+                           aktivitate=aktivitate)
 
 
 if __name__ == "__main__":
